a6ccff9fa8d1a26afb5247d04f1f77731ebdf30b
LucianC2025 <lucian.cruz2025@gbwl.org>
2025-01-22 15:42:09 +0000

    added layout template did webage wow

--- app.py
+++ app.py
@@ -16,6 +16,21 @@ def index():
 def hello_there(name_data = None):
     return render_template("hello_there.html", name=name_data)
 
+
+@app.route("/api/data/")
+def get_data():
+    # Displays data file returned from an API call
+    # can do more Python work to format this later
+    return app.send_static_file("data.json")
+
+@app.route("/about/")
+def about():
+     return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+     return render_template("contact.html")
+
 # Allows you to click "Run" button
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=1117)
